7d3a284470208bfd969025556b0194eb9675e9a1
salomelicheli <salome.licheli925@hum.tsu.ge>
2024-10-24 00:42:55 +0400

    string methods

---
+++ UserApp/StringProcessing/StringExtensions.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringProcessing
+{
+    public static class StringExtensions
+    {
+        public static string[] MySplit(this string s, char Flag = ' ')
+        {
+            int mark = 0;
+            int length = s.Length;
+            int newArrayLength = 1;
+            for (int i = 0; i < length; i++)
+            {
+                if (s[i] == Flag)
+                {
+                    newArrayLength++;
+                }
+            }
+            string[] newArray = new string[newArrayLength];
+            int k = 0;
+            for (int i = 0; i < length; i++)
+            {
+                if(s[i] == Flag)
+                {
+                    newArray[k] = s.Substring(mark, i - mark);
+                    mark = i + 1;
+                    k++;
+                }
+            }
+            newArray[k] = s.Substring(mark, length - mark);
+
+            return newArray;
+        }
+
+        public static bool MyContains(this string s, string word)
+        {
+            if (word == null)
+            {
+                throw new ArgumentNullException("The given word was null");
+            }
+                
+            for (int i = 0; i < s.Length - word.Length; i++)
+            {
+                if (s.Substring(i, word.Length) == word)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static string MyJoin(string[] s, string separator)
+        {
+            string NewString = "";
+            for (int i = 0; i < s.Length; i++)
+            {
+                NewString += s[i];
+                if (i != s.Length - 1)
+                {
+                    NewString += separator;
+                }
+            }
+            return NewString;
+        }
+
+        public static bool MyStartsWith(this string s, string word)
+        {
+            if(word.Length > s.Length)
+            {
+                return false;
+            }
+            bool StartsWith = true;
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (s[i] != word[i])
+                {
+                    StartsWith = false;
+                    break;
+                }
+            }
+            return StartsWith;
+        }
+
+        public static bool MyEndsWith(this string s, string word)
+        {
+            if (word.Length > s.Length)
+            {
+                return false;
+            }
+            bool EndsWith = true; 
+            int k = word.Length - 1; 
+
+            for (int i = s.Length - 1; i > s.Length - word.Length - 1; i--)
+            {
+                if (s[i] != word[k])
+                {
+                    EndsWith = false;
+                    break;
+                }
+                k--;
+            }
+            return EndsWith;
+        }
+    }
+}
---
+++ UserApp/StringProcessing/StringWrapper.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringProcessing
+{
+    internal class StringWrapper
+    {
+        private string Sentence { get; }
+
+        public int Length 
+        {
+            get
+            {
+                int index = 0;
+                foreach (char c in Sentence)
+                {
+                    index++;
+                }
+                return index;   
+            } 
+        }
+
+        public StringWrapper(string sentence)
+        {
+            Sentence = sentence;
+        }
+
+        public string MySubstring(int StartIndex, int SubLength)
+        {
+            if(StartIndex < 0 )
+            {
+                throw new ArgumentOutOfRangeException("The starting index can not be less than zero");
+            }
+            if (StartIndex > Length)
+            {
+                throw new ArgumentOutOfRangeException("The starting index can not be more than length");
+            }
+            if (SubLength > Length)
+            {
+                throw new IndexOutOfRangeException("The sublength cannot be more than length");
+            }
+            if (SubLength < 0)
+            {
+                throw new IndexOutOfRangeException("The sublength can not be less than zero");
+            }
+            string newStr = "";
+            for(int i = StartIndex; i < SubLength + StartIndex; i++)
+            {
+                newStr += Sentence[i];
+            }
+            return newStr;
+        }
+
+        public string MySubstring(int SubLength)
+        {
+            if (SubLength > Length)
+            {
+                throw new IndexOutOfRangeException("The sublength cannot be more than length");
+            }
+            if (SubLength < 0)
+            {
+                throw new IndexOutOfRangeException("The sublength can not be less than zero");
+            }
+            string newStr = "";
+            for (int i = Length - 1; i >= SubLength; i--)
+            {
+                newStr = Sentence[i] + newStr;
+            }
+            return newStr;
+        }
+
+        public int MyIndexOf(char c)
+        {
+            for(int i = 0; i < Length; i++)
+            {
+                if(Sentence[i] == c)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        public int MyIndexOf(string str)
+        {
+            int flag = str.Length;
+            for (int i = 0; i <= Sentence.Length - flag; i++)
+            {
+                if (Sentence.Substring(i, flag) == str)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        public int MyLastIndexOf(char c)
+        {
+            for(int i = Length - 1; i > 0; i --)
+            {
+                if (Sentence[i] == c)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        public int MyLastIndexOf(string str)
+        {
+            int flag = str.Length;
+            for(int i = Length - flag; i >= 0; i--)
+            {
+                if (Sentence.Substring(i, flag) == str)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        public string MyToUpper()
+        {
+            string str = "";
+            for (int i = 0;i < Length;i++)
+            {
+                char c = Sentence[i];
+                if(c >= 'a' && c <= 'z')
+                {
+                    c = (char)(c - 32);
+                } 
+                str += c;
+            }
+            return str;
+        }
+
+        public string MyToLower()
+        {
+            string str = "";
+            for (int i = 0; i < Length; i++)
+            {
+                char c = Sentence[i];
+                if (c >= 'A' && c <= 'Z')
+                {
+                    c = (char)(c + 32);
+                }
+                str += c;
+            }
+            return str;
+        }
+    }
+}
